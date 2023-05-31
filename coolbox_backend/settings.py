e51b9ad86fd094cca17fd66221a5f89894058c36
SleepyStew <sleepystew3@gmail.com>
2023-05-31 16:45:59 +1000

    Updated CSRF

--- coolbox_backend/settings.py
+++ coolbox_backend/settings.py
@@ -129,7 +129,7 @@ STATIC_URL = "static/"
 if not DEBUG:
     STATIC_ROOT = BASE_DIR / "static"
 
-CSRF_TRUSTED_ORIGINS = ["api.coolbox.lol"]
+CSRF_TRUSTED_ORIGINS = ["https://api.coolbox.lol"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
