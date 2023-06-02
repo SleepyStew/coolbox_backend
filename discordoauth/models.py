d40cad630d576f678ae14850d89d645c25d79147
SleepyStew <sleepystew3@gmail.com>
2023-06-02 19:10:06 +1000

    Fixed str and repr in DiscordOAuth Model

--- discordoauth/models.py
+++ discordoauth/models.py
@@ -10,7 +10,7 @@ class DiscordOAuth(models.Model):
     expires = models.IntegerField()
 
     def __str__(self):
-        return self.user
+        return self.user.name
 
     def __repr__(self):
-        return self.user
+        return self.user.name
