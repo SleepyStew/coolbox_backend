a266dd20fb7881cd3a066f1d488f96501abc27cd
SleepyStew <sleepystew3@gmail.com>
2023-06-02 17:25:00 +1000

    Added str and repr to Subject Model

--- subjects/models.py
+++ subjects/models.py
@@ -7,3 +7,9 @@ class Subject(models.Model):
     pretty = models.CharField(max_length=128, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return self.name
