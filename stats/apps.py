528c803431981868baad6c6b9a4e0c6c743ebea2
SleepyStew <sleepystew3@gmail.com>
2023-06-17 16:24:12 +1000

    Added startup debug statement

--- stats/apps.py
+++ stats/apps.py
@@ -1,6 +1,14 @@
+import os
+from pprint import pprint
+
 from django.apps import AppConfig
 
 
 class StatsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "stats"
+
+    def ready(self):
+        print("PRESTART")
+        if os.environ.get("RUN_MAIN", None) != "true":
+            print("STARTED")
