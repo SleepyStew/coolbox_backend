93a9f7b776d4a172df21f4d618b99fa03cdcd3d9
SleepyStew <sleepystew3@gmail.com>
2023-07-10 14:43:42 +1000

    Added timeout to sqlite

--- coolbox_backend/settings.py
+++ coolbox_backend/settings.py
@@ -92,7 +92,8 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
-    }
+        "OPTIONS": {"timeout": 5},
+    },
 }
 
 # Password validation
