5c5b101bb92277945d4a314c3ca78b7f18c9d46d
SleepyStew <sleepystew3@gmail.com>
2023-07-10 16:24:50 +1000

    Preparations for migration

--- coolbox_backend/settings.py
+++ coolbox_backend/settings.py
@@ -89,12 +89,22 @@ WSGI_APPLICATION = "coolbox_backend.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
+    "mysql": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": os.environ["DB_SCHEMA"],
+        "HOST": os.environ["DB_HOST"],
+        "PORT": 3306,
+        "USER": os.environ["DB_USER"],
+        "PASSWORD": os.environ["DB_PASSWORD"],
+    },
+    "sqlite": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
         "OPTIONS": {"timeout": 5},
     },
 }
+
+DATABASES["default"] = DATABASES[os.getenv("DB_TYPE", "sqlite")]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
