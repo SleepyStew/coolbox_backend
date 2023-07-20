485a57cb996b1a1a3948035e0b41bf737663e9f5
SleepyStew <sleepystew3@gmail.com>
2023-07-20 12:28:48 +1000

    Added RoomChanges and Forecast model to admin view

--- roomchanges/admin.py
+++ roomchanges/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from roomchanges.models import RoomChange
+
 # Register your models here.
+
+admin.site.register(RoomChange)
--- weather/admin.py
+++ weather/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from weather.models import Forecast
+
 # Register your models here.
+
+admin.site.register(Forecast)
