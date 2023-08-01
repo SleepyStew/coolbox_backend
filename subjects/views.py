3c860500c903d5c47daf3c328d9266b2db28518f
SleepyStew <sleepystew3@gmail.com>
2023-08-01 22:53:00 +1000

    Fixed server error on subjects endpoint

--- subjects/views.py
+++ subjects/views.py
@@ -34,5 +34,5 @@ class SubjectView(APIView):
                     subject_objects.append(subject_object)
 
         serializer = RetrieveSubjectSerializer(subject_objects, many=True)
-        not_blank_subjects = [subject for subject in serializer.data if subject.pretty != ""]
+        not_blank_subjects = [subject for subject in serializer.data if subject["pretty"] != ""]
         return Response(not_blank_subjects, status=status.HTTP_200_OK)
