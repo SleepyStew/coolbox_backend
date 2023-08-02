c695f4be095956e67ab235b89dfb1b48cfb803df
SleepyStew <sleepystew3@gmail.com>
2023-08-02 17:20:18 +1000

    Updated error reporting test case

--- errorreporting/tests.py
+++ errorreporting/tests.py
@@ -11,13 +11,19 @@ class ErrorReportViewTestCase(APITestCase):
     def test_subject_view(self):
         error_report_endpoint = "/error-report"
 
-        error = {
+        errors = [{
             "error": "Test",
             "detail": "Detail",
-        }
 
-        response = self.client.post(error_report_endpoint, error, format="json")
+        },
+            {
+                "error": "Test2",
+                "detail": "Detail2",
+            }
+        ]
+
+        response = self.client.post(error_report_endpoint, errors, format="json")
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data["error"], error["error"])
-        self.assertEqual(response.data["detail"], error["detail"])
+        self.assertEqual(response.data[0]["error"], errors[0]["error"])
+        self.assertEqual(response.data[1]["detail"], errors[1]["detail"])
