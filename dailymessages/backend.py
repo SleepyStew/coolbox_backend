fe09f14a4e9e9d3caee7cd65c3202a160d18ed21
SleepyStew <sleepystew3@gmail.com>
2024-02-04 17:30:56 +1100

    Fixed Daily Verse including unwanted HTML
    Also changed to NLT, will most likely be customizable in the future.

--- dailymessages/backend.py
+++ dailymessages/backend.py
@@ -11,13 +11,18 @@ def get_daily_verse():
     from .models import DailyVerse
 
     daily_verse_request = requests.get(
-        "https://www.biblegateway.com/votd/get/?format=json&version=NIV"
+        "https://www.biblegateway.com/votd/get/?format=json&version=NLT"
     )
+
     if daily_verse_request.status_code != 200:
         return
 
     daily_verse = daily_verse_request.json()["votd"]
     DailyVerse.objects.all().delete()
     DailyVerse.objects.create(
-        content=html.unescape(daily_verse["content"]), reference=daily_verse["reference"], link=daily_verse["permalink"]
+        content=html.unescape(
+            daily_verse["text"].replace("&ldquo;", "").replace("&rdquo;", "")
+        ),
+        reference=daily_verse["reference"],
+        link=daily_verse["permalink"],
     )
